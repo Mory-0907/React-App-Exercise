be7ee751ff364e345c93b228aeb9914d76e60c89
Morena D'Asta <morena.work.0907@gmail.com>
2025-04-08 17:06:47 +0200

    esercizio 2 svolto

---
+++ Exercise-2/src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import Message from "./Message";
+function HelloWorld() {
+  return( <div><h2>Hello World!</h2>
+    <div><Message/></div></div>
+  )
+}
+
+export default HelloWorld;
+
+
+
---
+++ Exercise-2/src/Message.jsx
@@ -0,0 +1,5 @@
+function Message(){
+    return <p>Che bella giornata!</p>
+}
+
+export default Message;
