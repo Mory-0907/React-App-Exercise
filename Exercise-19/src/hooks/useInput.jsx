188a67b3b45f85db06da632aba5804e406c08365
Morena D'Asta <morena.work.0907@gmail.com>
2025-04-30 19:25:27 +0200

    creazione degli hooks

---
+++ Exercise-19/src/hooks/useInput.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export function useInput() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  return { formData, handleChange };
+}
