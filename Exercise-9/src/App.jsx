200a61f6abb7f622585eba2dcbc767ed88a43b3d
Morena D'Asta <morena.work.0907@gmail.com>
2025-05-06 18:12:21 +0200

    A new React App 3, add modified  Exercise-9

--- Exercise-9/src/App.jsx
+++ Exercise-9/src/App.jsx
@@ -1,50 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Messaggio from "./Messaggio";
 import "./styles.css";
 
 function App() {
-
   const [messages, setMessages] = useState([
     { text: "Ciao Morena", className: "" },
     { text: "Ciao!", className: "blu-right" },
     { text: "Come stai?", className: "" },
     { text: "Tutto bene, grazie", className: "blu-right" },
-    { text: "Fammi sapere se ci sarai all'evento programmato per venerdì", className: "" },
+    {
+      text: "Fammi sapere se ci sarai all'evento programmato per venerdì",
+      className: "",
+    },
     { text: "Ok, ci aggiorniamo, buona giornata!", className: "blu-right" },
   ]);
 
+  const chatEndRef = useRef(null); 
+
+  useEffect(() => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" }); 
+  }, [messages]);
 
   const [newMessage, setNewMessage] = useState("");
 
-  
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
 
     if (newMessage.trim() === "") return;
 
     setMessages([...messages, { text: newMessage, className: "blu-right" }]);
 
-   
     setNewMessage("");
 
-  
     setTimeout(() => {
       const randomResponses = [
         "scusa ora devo andare",
         "eh perdonami devo proprio andare",
         "sono un po' occupato ora parliamo dopo?",
         "devo veramente andare via...",
-        "scusami...devo andare"
+        "scusami...devo andare",
       ];
-     
-      const randomResponse = randomResponses[Math.floor(Math.random() * randomResponses.length)];
 
-     
+      const randomResponse =
+        randomResponses[Math.floor(Math.random() * randomResponses.length)];
+
       setMessages((messaggiPrecedenti) => [
         ...messaggiPrecedenti,
-        { text: randomResponse, className: "" } 
+        { text: randomResponse, className: "" },
       ]);
-    }, 2000); 
+    }, 2000);
+   
   };
 
   return (
@@ -52,13 +57,12 @@ function App() {
       <section>
         <main>
           <h1>APPLICAZIONE REACT</h1>
-        
-        <hr />
+
+          <hr />
           {messages.map((msg, index) => (
             <Messaggio key={index} text={msg.text} className={msg.className} />
           ))}
 
-      
           <form onSubmit={handleSubmit}>
             <input
               type="text"
@@ -70,12 +74,9 @@ function App() {
             <input type="submit" value="Invia" />
           </form>
         </main>
-        
       </section>
     </>
   );
 }
 
 export default App;
-
-
--- Exercise-9/src/Messaggio.jsx
+++ Exercise-9/src/Messaggio.jsx
@@ -3,14 +3,12 @@ import "./messaggio.css";
 const Messaggio = (props) => {
   return (
     <div className="container">
-      
       {props.className === "blu-right" ? (
         <>
           <p className={props.className}>{props.text}</p>
           <img className="imgReact" src="/assets/con sfondo.png" alt="me" />
         </>
       ) : (
-      
         <>
           <img className="imgReact" src="/assets/react.png" alt="react logo" />
           <p className={props.className}>{props.text}</p>
