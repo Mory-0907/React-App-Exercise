bd86946f27980de9390fcdbf922851dd47d6e4e0
Morena D'Asta <morena.work.0907@gmail.com>
2025-05-11 17:53:23 +0200

    SWR

--- Exercise-22/src/hooks/useGithubUser.js
+++ Exercise-22/src/hooks/useGithubUser.js
@@ -1,21 +1,15 @@
 import useSWR from "swr";
 
-function useGithubUser(username) {
+export function useGithubUser(username) {
   const shouldFetch = Boolean(username);
-  const { data, error, isLoading, mutate } = useSWR(
+  const { data, error, loading, mutate } = useSWR(
     shouldFetch ? `https://api.github.com/users/${username}` : null
   );
 
-  const refetch = () => {
-    if (shouldFetch) mutate();
-  };
-
   return {
     user: data,
-    error,
-    loading: isLoading,
-    refetch
+    isLoading: loading,
+    isError: error,
+    refetch: mutate,
   };
 }
-
-export default useGithubUser;
